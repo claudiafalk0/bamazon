c697c2f6f472c823be40fd3e36c67c78f1910fa4
Claudia Falk <claudiafalk0@gmail.com>
2019-07-01 18:48:03 -0600

    added table, inquirer prompts'

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+var table = require('cli-table');
+var divider = '\n---------------------\n\n'
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 8889,
@@ -11,5 +12,55 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     if (err) throw err;
-    console.log(connection.threadId);
-})
+    readTable();
+});
+function readTable(){
+
+    connection.query('SELECT * FROM items', function(err, res){
+        if (err) throw err;
+       var displayTable = new table({
+        head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity'],
+        colWidths: [10,25,25,10,14]
+       });
+       for (i = 0; i < res.length; i++){
+           displayTable.push(
+               [res[i].id, res[i].name, res[i].department_name, res[i].price, res[i].quantity]
+           );
+       };
+       console.log(displayTable.toString());
+       divider;
+       questions();
+
+    })
+}
+function updateProduct(){
+    console.log
+}
+
+function questions(){
+    inquirer.prompt([
+        {
+            name: 'id_product',
+            message: 'Please enter the id of the item you would like to purchase',
+            validate: function(value){
+                if(isNaN(value)){
+                    console.log(`\n Please enter an item number\n\n`)
+                }else if(value != res.id){
+                    console.log(`\nplease enter a valid id\n\n`);
+                }
+            }
+
+        },{
+            name:'how_many',
+            message: 'Please enter the number of items you would like to purchase'
+
+        }
+    ]).then(function(answer){
+        if(answer.quantity>='how_many'){
+            console.log(`You have purchased ${how_many} ${id_product}`);
+        }else{
+            console.log('Insufficient quantity! Please enter a new amount.');
+        };
+
+    })
+}
--- bamazon.sql
+++ bamazon.sql
@@ -7,11 +7,13 @@ USE bamazon_DB;
 CREATE TABLE items (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
+    department_name VARCHAR(100),
+    price DECIMAL (10,2),
     quantity INT,
     PRIMARY KEY (id)
 );
 
 INSERT INTO items (name, quantity)
-VALUES ('Partridge in a pear tree', 1), ('Maids a"milking', 6), ('gold rings', 5);
+VALUES ('Partridge in a pear tree', 1), ('Maids a"milking', 8), ('gold rings', 5), ('swans a"swimming', 7), ('Calling birds', 4), ('French Hens', 3), ('Turtle Doves', 2), ('Geese a"laying', 6), ('Lords a"leaping', 10), ('Ladies Dancing', 9);
 
 SELECT * FROM items;
